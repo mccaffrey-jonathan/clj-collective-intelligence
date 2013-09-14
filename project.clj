12266fa7fcd446accc47cfaac42bde7ac8d09990
jmccaf <mccaffrey.jonathan@gmail.com>
2013-09-14 15:04:26 -0700

    add more library depencies

---
+++ project.clj
@@ -0,0 +1,6 @@
+(defproject clj-collective-intelligence "0.1.0-SNAPSHOT"
+  :plugins [[lein-midje "3.0.0"]]
+  :source-paths ["src"]
+  :dependencies [[midje "1.5.1"] 
+                 [org.clojure/clojure "1.4.0"]
+                 [org.clojure/tools.nrepl "0.2.3"]])
